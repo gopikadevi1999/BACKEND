107f2b4bd44b665d8eff4c186d0638674bd3605c
gopikadevi1999 <gopikadevi6363@gmail.com>
2024-03-22 23:14:11 +0530

    cors updated

--- app.js
+++ app.js
@@ -11,7 +11,10 @@ const PaymentRoutes = require('./router/payment.js')
 
 const app = express();
 
-app.use(cors())
+app.use(cors({
+    origin:['https://rsr-kappa.vercel.app/', 'http://localhost:5173'],
+    credentials: true
+}))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 app.use(cookieParser())
